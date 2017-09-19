711fdc7f49a306cc78c92127a5573b5614d6df01
tpogrebniak <tpogrebniak@magento.com>
2017-09-19 17:09:24 +0300

    Init commit

---
+++ app/code/community/Sywebo/Ajaxcart/Block/Popup.php
@@ -0,0 +1,24 @@
+<?php
+class Sywebo_Ajaxcart_Block_Popup extends Mage_Checkout_Block_Cart
+{
+    public function getContinueShoppingMessage()
+    {
+        return Mage::helper('checkout')->__("Continue Shopping");
+    }
+
+   public function getCheckoutMessage()
+   {
+       return Mage::helper('checkout')->__("Proceed to Checkout");
+   }
+
+   public function getProductWasAddedMessage()
+   {
+       return Mage::helper('checkout')->__("was added to cart");
+   }
+
+   public function getProductMessage()
+   {
+       return Mage::helper('checkout')->__("Product");
+   }
+
+}
